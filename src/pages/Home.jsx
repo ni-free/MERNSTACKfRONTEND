18da83a1c88331868ba7a9274962e982ce230ff8
ni-free <nidhigoyal6146@gmail.com>
2024-03-31 10:42:44 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { todo } from '../images/index.js'
+import { Link } from 'react-router-dom'
+import Login from './Login.jsx'
 const Home = () => {
   return (
    <section className='bg-black top-0 fixed w-[100%] ml-[0px]'  ><div className='h-[100%] w-[100%] bg-black text-white flex top-0'>
@@ -7,7 +9,8 @@ const Home = () => {
     <h2 className='sm:text-5xl text-2xl font-semibold sm:leading-snug font-poppins text-left'>Become your most <br /> productive self with <br /> TODO</h2>
      <h3 className='mt-4 mb-4 text-white-300 font-semibold text-left'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam labore ducimus dolorem adipisci obcaecati id tenetur ut non quos magnam.</h3>
      
-     <button className='btn '><a href='/login'>Lets Start</a></button>
+     <button className='btn '><Link to={<Login
+    />}></Link> </button>
   </div>
   <div className='h-[50vh] w-[220px]   mt-44 mr-32 mb-20 ml-20'>
     <img src={todo} alt="bg" />
